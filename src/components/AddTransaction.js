567ae98b6e1a48a90a12ad6eb6d9cc768175bba0
Jonathan Zavala <jontzavala@gmail.com>
2022-06-10 12:07:07 -0500

    finished delete transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const AddTransaction = () => {
 
@@ -6,10 +7,23 @@ export const AddTransaction = () => {
     const [amount, setAmount] = useState(0)
 
 
+    const {addTransaction} = useContext(GlobalContext)
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount
+        }
+    }
+
+
   return (
     <>
         <h3>Add New Transaction</h3>
-        <form>
+        <form onSubmit={onSubmit}>
             <div className='form-control'>
                 <label htmlFor='text'>Text</label>
                 <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...'/>
